43d9213698d90f62f3d128e055785c08a751bf31
Ravicha <palm.ravicha@outlook.com>
2024-08-09 19:36:19 +0700

    test connection with API

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // index.js
-console.log("js is working today")
+//test API by GET ID1
+$(document).ready(function () {
+    console.log('DOM is ready');
+    $.ajax({
+        type: 'GET',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+        dataType: 'json',
+        success: function (response, textStatus) {
+            response.tasks.forEach(function (task) {
+                console.log(task.content);
+            });
+            
+          
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });
+});
