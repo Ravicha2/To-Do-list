e24afd40f0afd7a33333f72186fe0eabf068f807
Ravicha <palm.ravicha@outlook.com>
2024-08-10 15:54:48 +0700

    add POST request

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 // index.js
 //test API by GET ID1
 $(document).ready(function () {
-    console.log('DOM is ready');
+    //console.log('DOM is ready');
     $.ajax({
         type: 'GET',
-        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1269',
         dataType: 'json',
         success: function (response, textStatus) {
             response.tasks.forEach(function (task) {
-                console.log(task.content);
-            });
-            
-          
+                $('#todo-list').append('<p>'+task.content+'</p>');
+            })
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+    });
+    
+    $.ajax({
+        type: 'POST',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1269',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          task: {
+            content: 'Do something fun'
+          }
+        }),
+        success: function (response, textStatus) {
+          console.log(response);
         },
         error: function (request, textStatus, errorMessage) {
           console.log(errorMessage);
